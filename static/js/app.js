1419beea9d670d380735aba524d7d8ce9e566f3a
Nick Eccher <nicholas.eccher@owen.alumni.vanderbilt.edu>
2021-05-19 13:08:40 -0500

    11.5.3 Push

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,22 @@ const  tableData = data;
 
 // reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+// function to build our table
+function buildTable(data) {
+    //Clear out any existing data
+    tbody.html("");
+
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+
+        // Loop through each field in the dataRow and add
+        // each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        })
+    });
+}
